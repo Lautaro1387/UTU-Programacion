a70d217dc4dceeb591aba82d4c327c7a609b5167
tecnoinf <tecnoinf@tecnoinf203.esi.edu.uy>
2026-04-09 10:33:25 -0300

    Add eje1 practico1 a,b,c,d not finish

--- UTU-PP/practico-2/eje1.cpp
+++ UTU-PP/practico-2/eje1.cpp
@@ -7,6 +7,7 @@ int main() {
 int numero[TAM];
 int i;
 int promedio;
+int valor;
 
 // a) Cargar un arreglo del tipo definido con valores leídos desde teclado.
 
@@ -21,23 +22,29 @@ for (i = 0; i < TAM; i++){
 }
 printf("\n");
 
-// c) Calcular el promedio de los valores almacenados en el arreglo.
-for (i = 0; i < TAM; i++){
+// c) Calcular el promedio de los valores almacenados en el arreglo - CHECK.
+for (i = 0; i < TAM; i++){  
 
-
-
+    promedio = promedio + numero[i];
+    promedio = promedio / numero[i];
 }
+printf("El promedio es: %d", promedio);
 
-}
 
 // d) Leer un valor entero desde teclado y determinar si pertenece o no al arreglo.
-// e) Encontrar el menor valor y el mayor valor almacenados en el arreglo, junto con las
-// posiciones correspondientes.
+
+for (i = 0; i < TAM; i++) {
+    
+    printf("Ingresa un valor entero: ", valor);
+    scanf("%d", &valor);
+    
+}
+
+}
+// e) Encontrar el menor valor y el mayor valor almacenados en el arreglo, junto con las posiciones correspondientes.
 // f) Leer un valor desde teclado y contar cuántos múltiplos de dicho valor hay en el arreglo.
-// g) Leer un valor entero desde teclado y mostrar por pantalla los valores del arreglo que estén
-// almacenados en celdas cuyo índice (posición) sea múltiplo del valor leído.
-// h) Invertir el arreglo sobre sí mismo (sin usar otro arreglo auxiliar) y desplegarlo por pantalla
-// luego de invertido.
+// g) Leer un valor entero desde teclado y mostrar por pantalla los valores del arreglo que estén almacenados en celdas cuyo índice (posición) sea múltiplo del valor leído.
+// h) Invertir el arreglo sobre sí mismo (sin usar otro arreglo auxiliar) y desplegarlo por pantalla luego de invertido.
 // i) Buscar los dos enteros menores del primer arreglo y desplegarlos por pantalla.
 // j) Cargar otro arreglo del tipo definido, ordenándolo a medida que se leen los números.
 // Desplegar por pantalla el contenido del arreglo.
