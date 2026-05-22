54f0e408cfcdb2044d663f658799a1124c58e1a0
Lautaro1387 <lautaro12387@gmail.com>
2026-05-22 09:34:52 -0300

    Fix

--- UTU-PP/lab/part1.cpp
+++ UTU-PP/lab/part1.cpp
@@ -2,6 +2,9 @@
 #include <stdbool.h>
 #include <stdlib.h> // rand - srand
 #include <time.h>
+#include <unistd.h>
+// sleep(2);
+// system("clear");
 
 const int N = 100; // nombre;
 // Largo de la secuencia de colores dependiendo el nivel
@@ -20,28 +23,16 @@ const int ACIERTO_NIVEL_A = 5;
 
 int main(){
 
-
-
-
-
-
-
-
-
-
-
-
-    int i, puntaje, largo;
-    float tiempo;
+    int i, puntaje, randoms, j;
     char nombre[100];
-    char c,h,nivel;
+    char h,nivel;
     char centinela[]={'f','i','n','\0'};
     bool t = true, encontrada = true;
-    // Generar un número aleatorio y limitarlo al rango de 0 a 99
+    // Generar un número aleatorio y limitarlo al rango de 0 a 3
     srand(time(NULL));
-    int randoms = rand() % 4;
+    randoms = rand() % 4;
 
-     
+
     printf("Bienvenido al Juego de SIMON ver. 1.0.\n");
     printf("Ingrese su nombre\n");
     i=0;
@@ -65,67 +56,84 @@ int main(){
        // do while
             printf("Ingrese nivel del Juego (p, i, a): ");
             scanf(" %c",&nivel);
-            printf("\n");
+        
             switch(nivel)
             {
-                case 'p':case 'P' :printf("Pulse <enter> para comenzar el juego.\n");
-                                   scanf("%c",&h);
-                                   while(h!='\n')
+                case 'p':case 'P' :
+                                    printf("Pulse <enter> para comenzar el juego.");
+                                    
+                                    while (h!='\n') {
                                     scanf("%c",&h);
-                                   char secuencia[NIVEL_P];
-                                   char ingreso[NIVEL_P];
-                                   for (i=0; i < NIVEL_P; i++) { // 
-                                       switch (randoms)
-                                       {
-                                           case 0: 
-                                           secuencia[i]='R';
+                                }
+                                scanf("%c",&h);
+                                
+                                
+                                
+                                char secuencia[NIVEL_P];
+                                char ingreso[NIVEL_P];
+                                for (i=0; i < NIVEL_P; i++) { //
+                                    switch (randoms)
+                                    {
+                                        case 0:
+                                        secuencia[i]='R';
                                            break;
-                                           case 1: 
+                                           case 1:
                                            secuencia[i]='A';
                                            break;
-                                           case 2: 
+                                           case 2:
                                            secuencia[i]='V';
                                            break;
-                                           case 3: 
+                                           case 3:
                                            secuencia[i]='N';
                                            break;
                                            
                                         }
-
-                                    }
-                                    do {
-                                        printf("%c", secuencia[i]);
-                                        scanf("%c", ingreso[i]);
-                                        if (secuencia[i] == ingreso[i])
-                                            i++;
-                                        else
+                                    }       
+                                    do { 
+                                            printf("%c \n", secuencia[j]);
+                                            //sleep(TIEMPO_COLOR_P);
+                                            // system("clear");
+                                            scanf(" %c \n", &ingreso[j]);
+                                            if (secuencia[j] == ingreso[j])
+                                            {
+                                                j++;
+                                                puntaje = puntaje + ACIERTO_NIVEL_P;
+                                                printf("test - correcto");
+                                            }
+                                        else {
                                             encontrada = false;
-                                    } while(i < NIVEL_P && encontrada);
-                                   
-                                   puntaje=ACIERTO_NIVEL_P;
-                                   tiempo=TIEMPO_COLOR_P;                               
-                                   break;
+                                            printf("test - incorrecto");
+                                            }
+                                        
+                                    } while(encontrada && j < NIVEL_P );
+                                break;
+                                t=false;
+
+                                            // Problema actual en secuencia[i] queda el valor de f y si lo detecta como correcto si coloco ese valor
+                                            
+                                            // 
+                                            
+                                           
+
                 case 'i':case 'I' : printf("Pulse <enter> para comenzar el juego.\n ");
                                    scanf("%c",&h);
                                    while(h!='\n')
                                     scanf("%c",&h);
                                    secuencia[NIVEL_I];
-                                   puntaje=ACIERTO_NIVEL_I;
-                                   tiempo=TIEMPO_COLOR_I;
                                    break;
                 case 'a':case 'A' :printf("Pulse <enter> para comenzar el juego.\n ");
                                    scanf("%c",&h);
                                    while(h!='\n')
                                     scanf("%c",&h);
                                    secuencia[NIVEL_A];
-                                   puntaje=ACIERTO_NIVEL_A;
-                                   tiempo= TIEMPO_COLOR_A;
                                    break;
                 default : printf("Caracter incorrecto, ingrese otro\n");
                           break;
             }
-             
+            printf("%c %d", puntaje);
+
+
 
     }
-}
+} 
 
